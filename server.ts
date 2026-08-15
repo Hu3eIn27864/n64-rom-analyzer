7b4afeef6af62b26ef213bf3a11a55da2ab5e281
Hu3eIn27864 <hussein.sahabi@yahoo.com>
2026-08-16 01:07:34 +0200

    Update server.ts

--- server.ts
+++ server.ts
@@ -3,6 +3,7 @@ import path from 'path';
 import { createServer as createViteServer } from 'vite';
 import { GoogleGenAI } from '@google/genai';
 import dotenv from 'dotenv';
+import { analyzeRom } from './engine/pipeline';
 
 dotenv.config();
 
